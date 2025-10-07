48a84ab06ec4cdcd783312fed61c873c4b6bd434
Rahul Pal <rahulpal21@gmail.com>
2025-10-07 17:47:07 -0400

    initial skeleton

---
+++ src/main/java/com/github/rahulpal21/kafkamplify/KafkamplifyApplication.java
@@ -0,0 +1,13 @@
+package com.github.rahulpal21.kafkamplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkamplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkamplifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rahulpal21/kafkamplify/KafkamplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rahulpal21.kafkamplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkamplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
